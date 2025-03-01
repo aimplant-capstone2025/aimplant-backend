5810bd346776fa3cf0653507860341400536c694
Nathathaii <nathathaiaungudomsin@gmail.com>
2025-03-01 14:11:30 +0700

    add default path/

--- main.py
+++ main.py
@@ -118,6 +118,12 @@ async def predict(file: UploadFile = File(...)):
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
 
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, AImplant backend is alive!"}
+
+
 # Run the FastAPI server
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
